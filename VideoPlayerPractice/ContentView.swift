9448ee1064a24fa658a121061ca39bbf3b572510
Ayush Singhal <ayush.singhal@magicpin.in>
2025-06-14 13:32:55 +0530

    Initial Commit

---
+++ VideoPlayerPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VideoPlayerPractice
+//
+//  Created by magicpin on 14/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VideoPlayerPractice/VideoPlayerPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerPracticeApp.swift
+//  VideoPlayerPractice
+//
+//  Created by magicpin on 14/6/25.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
